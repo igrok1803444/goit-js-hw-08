b96f989e8472db95a51cfa182c6b2b95c27866c1
igrok1803444 <v618591@gmail.com>
2023-08-11 13:22:52 +0300

    end task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+//імпорт модуля
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//плеер
+const player = new Player('vimeo-player');
+
+player.on('timeupdate',  throttle(() => {
+   player.getCurrentTime().then(function(seconds) {
+    // seconds = the current playback position
+       localStorage.setItem('videoplayer-current-time', seconds);
+   
+}).catch(function(error) {
+    // an error occurred
+});
+}, 1000))
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+})
+
 
